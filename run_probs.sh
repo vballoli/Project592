a08a5b103529702fb63c4984cb0f973673b9f525
Vaibhav Balloli <vballoli@gl-login1.arc-ts.umich.edu>
2024-04-04 14:37:46 -0400

    Code until progress

---
+++ run_probs.sh
@@ -0,0 +1,14 @@
+bash run.sh run_main.sh joint_1 0.0 0.2 & 
+bash run.sh run_main.sh joint_2 0.1 0.2 &
+bash run.sh run_main.sh joint_3 0.2 0.2 &
+bash run.sh run_main.sh joint_4 0.3 0.2 &
+bash run.sh run_main.sh joint_5 0.4 0.2 &
+bash run.sh run_main.sh joint_6 0.5 0.2 &
+bash run.sh run_main.sh joint_7 0.6 0.2 &
+bash run.sh run_main.sh joint_8 0.7 0.2 &
+
+bash run.sh run_main.sh joint_40 0.3 0.0 &
+bash run.sh run_main.sh joint_42 0.3 0.2 &
+bash run.sh run_main.sh joint_44 0.3 0.4 &
+bash run.sh run_main.sh joint_46 0.3 0.6 &
+
