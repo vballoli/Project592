0b6a212c8bcfdb02046a9fe3ad35cb72e3f68ae3
Vaibhav Balloli <vballoli@gl-login1.arc-ts.umich.edu>
2024-04-04 14:37:46 -0400

    Code until progress

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+script_name=$1
+job_name=$2
+prob=$3
+alpha=$4
+
+sbatch --partition=spgpu --gpus=1 --cpus-per-gpu=8 --nodes=1 --mem-per-cpu=11500m --time=00-24:00:00 --job-name=$job_name --output="logs/$job_name.out" $script_name $prob $alpha
---
+++ run_main.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python main.py --config ./configs/config_exp.yaml --prob $1 --alpha $2
