64f38553ac2a90c945655fb1998b640cae895738
Zariq George <georgz@umich.edu>
2024-04-23 19:56:54 -0400

    script for second set of experiments and updated sbatch script

---
+++ run_experiments_two.sh
@@ -0,0 +1,44 @@
+#Regular CBM Experiments
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_16 0.0 0.6 regcbm & 
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_26 0.1 0.6 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_36 0.2 0.6 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_46 0.3 0.6 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_56 0.4 0.6 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_66 0.5 0.6 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_76 0.6 0.6 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_86 0.7 0.6 regcbm &
+
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_14 0.0 0.4 regcbm & 
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_24 0.1 0.4 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_34 0.2 0.4 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_44 0.3 0.4 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_54 0.4 0.4 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_64 0.5 0.4 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_74 0.6 0.4 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_84 0.7 0.4 regcbm &
+
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_18 0.0 0.8 regcbm & 
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_28 0.1 0.8 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_38 0.2 0.8 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_48 0.3 0.8 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_58 0.4 0.8 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_68 0.5 0.8 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_78 0.6 0.8 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_88 0.7 0.8 regcbm &
+
+
+
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_30 0.3 0.0 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_32 0.3 0.2 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_34 0.3 0.4 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_36 0.3 0.6 regcbm &
+
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_50 0.5 0.0 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_52 0.5 0.2 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_54 0.5 0.4 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_56 0.5 0.6 regcbm &
+
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_70 0.7 0.0 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_72 0.7 0.2 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_74 0.7 0.4 regcbm &
+bash ./greatLakes_scripts/run.sh ./greatLakes_scripts/run_model.sh joint_76 0.7 0.6 regcbm &
